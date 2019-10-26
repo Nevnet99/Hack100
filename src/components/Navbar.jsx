abfcceb563496536306968a4512895071a4d13d8
Nevnet99 <luke-brannagan@hotmail.com>
2019-10-26 16:17:47 +0100

    main-page2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,17 @@ import "../styles/Navbar.css";
 function Navbar() {
   return (
     <div className="Navbar">
+      <div className="heroImage"></div>
       <div className="Logo">Eventifind</div>
       <div className="ListOfButtons">
         <button> Home </button>
         <button> Profile </button>
         <button> Edit my preferences. </button>
       </div>
+      {/* <div className="Login-Section">
+        <button> Signup </button>
+        <button> Login </button>
+      </div> */}
     </div>
   );
 }
