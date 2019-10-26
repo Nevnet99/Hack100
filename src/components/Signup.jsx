106afd7f550727fe6b000d7fe2ac2d1de41383ff
Nevnet99 <luke-brannagan@hotmail.com>
2019-10-26 20:07:40 +0100

    signup_and_userpref

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Signup.css";
 
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = e => {
+    // Need to hook up to backend.
+    console.log("Submitted :) ");
+  };
+  const handleChange = e => {
+    if (e.target.name === "username") {
+      setUsername(e.target.value);
+    } else if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
+  };
+
   return (
     <div className="Signup">
-      <h1>Signup</h1>
-      <label>
-        Username:
-        <input
-          className="usernameInput"
-          placeholder="Enter your Username..."
-          type="text"
-          name="username"
-        />
-      </label>
+      <div className="userProfile">
+        <label className="usernameInput">
+          USERNAME:
+          <input
+            placeholder="Enter your Username..."
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
 
-      <label>
-        Password:
-        <input
-          className="passwordInput"
-          placeholder="Enter your Password..."
-          type="text"
-          name="password"
-        />
-      </label>
+        <label className="passwordInput">
+          PASSWORD:
+          <input
+            placeholder="Enter your Password..."
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <button onSubmit={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 }
