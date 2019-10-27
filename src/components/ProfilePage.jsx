fedacfbbfbfd7c6ce4b6a403b4637c657a2af439
Nevnet99 <luke-brannagan@hotmail.com>
2019-10-27 03:26:37 +0000

    user profile is now styled.

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,28 +1,44 @@
 import React from "react";
-import person from "../assets/person.jpg"
+import person from "../assets/person.jpg";
 import "../styles/ProfilePage.css";
 
-
 function ProfilePage() {
-    return (
-<div className="back">
-<h1 className="hello"> Hello ${'User'}</h1>
-<div className="container">
-    <img src={person} alt="Avatar" className="image" />
+  return (
+    <div className="background">
+      <div className="profileContainer">
+        <div className="profileAside">
+          <div className="imageContainer">
+            <img src={person} alt="Avatar" className="image" />
+          </div>
+          <div className="helloSection">
+            <h1 className="hello"> Hi {"Tony"}</h1>
+            <p>
+              Hi my name is Tony, Im a QA tester at heart. <br />
+              I love long walks in the server room, as well <br />
+              as attending the best meetups that Manchester <br />
+              has to offer.
+            </p>
+          </div>
+        </div>
+
+        <div className="profileInfo">
+          <p>Full Name: Tony Tester</p>
+          <p>Email: Tony@Tester.com</p>
+          <p>Mobile: 0000 0000 000</p>
+          <hr />
+          <div>
+            <h2>Likes</h2>
+            <div>QA Testing</div>
+            <div>Hackathons</div>
+            <div>Memes</div>
+          </div>
+          <button className="customProfileButton">
+            Recieve SMS messages for events near you.
+          </button>
+        </div>
+      </div>
     </div>
-    <div className="text">The user's name imported here</div>
-
-  
-<h3 className="email"> Email </h3> 
-<h3 className="interests"> Interests </h3>
-<ul className="list">
-    <li> </li>
-    <li> </li>
-    <li> </li>
-</ul>
-<button className="button"> Text me an event of interest </button>
-</div>
-
-    )};
+  );
+}
 
 export default ProfilePage;
